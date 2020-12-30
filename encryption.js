91d63845d636138b7edef18d162b17cb8ec840fe
Mirko Ventura <mirko.ventura87@gmail.com>
2020-12-30 11:31:14 +0100

    add "accees" observer

--- encryption.js
+++ encryption.js
@@ -86,4 +86,52 @@ module.exports = function (Model, options) {
             });
     });
 
+    Model.observe('access', function event(ctx, next){
+
+        var iv = Buffer.from(options.hexIv || defaultOpt.hexIv, "hex");
+        crypto.pbkdf2(options.password || defaultOpt.password,
+            options.salt || defaultOpt.salt,
+            options.iteration || defaultOpt.iteration,
+            options.hashBytes || defaultOpt.hashBytes,
+            options.hashAlgorithm || defaultOpt.hashAlgorithm,
+            function (err, derivedKey) {
+            if (err) {
+                console.log(err);
+                next(err);
+            }
+            else {
+                var fields = options.fields || defaultOpt.fields;
+                try{
+                    for (var i in fields) {
+                        var queryParam = ctx.query.where[fields[i]];
+                        if(queryParam === undefined){
+                            continue;
+                        }
+                        if (queryParam.constructor.name === "Object") {
+                            var keys = Object.keys(queryParam);
+                            for (var j in keys) {
+                                var cipher = crypto.createCipheriv(options.encryptionAlgorithm || defaultOpt.encryptionAlgorithm, derivedKey, iv);
+                                var crypted = cipher.update(toCrypt, 'utf8', 'hex');
+                                crypted += cipher.final('hex');
+                                queryParam[keys[j]] = crypted;
+                            }
+                            ctx.query.where[fields[i]] = queryParam;
+                        }else{
+                            var cipher = crypto.createCipheriv(options.encryptionAlgorithm || defaultOpt.encryptionAlgorithm, derivedKey, iv);
+                            var crypted = cipher.update(ctx.query.where[fields[i]], 'utf8', 'hex');
+                            crypted += cipher.final('hex');
+                            ctx.query.where[fields[i]] = crypted;
+                        }
+                    }
+                }catch (ex){
+                    ex.message += "\nThis usually happens when the field contains a plain text! please make sure to remove/re-save that";
+                    console.log(ex.message);
+                }finally {
+                    console.log('Crypted %s matching %s', ctx.Model.modelName, ctx.query.where);
+                    next();
+                }
+            }
+        });
+    });
+
 };
